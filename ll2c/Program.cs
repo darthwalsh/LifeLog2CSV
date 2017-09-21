f6afcd5c0f8eaf3d0e0fa4664295276cb33e3780
Carl Walsh <darthwalsh@gmail.com>
2017-09-20 22:54:53 -0700

    project framework

---
+++ ll2c/Program.cs
@@ -0,0 +1,17 @@
+﻿using llParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ll2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Run(@"C:\Users\darth\Downloads\Screenshot_20170906-100304.png");
+        }
+    }
+}
