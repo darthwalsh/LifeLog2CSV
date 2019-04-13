0edeb5518e09890f1794d913b9cb2c488314db54
Carl Walsh <darthwalsh@gmail.com>
2019-04-12 19:50:09 -0700

    GUI fixups for path and preventing redraw
    Generalize bitmap path
    Stop redrawing the bitmap after parsing is done

--- LifeLogGUI/ParserForm.cs
+++ LifeLogGUI/ParserForm.cs
@@ -2,21 +2,22 @@
 using System.Drawing;
 using LifeLogParser;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LifeLogGUI
 {
     public partial class ParserForm : Form
     {
-        const int fps = 1000 / 60;
-        Bitmap image;
-        Timer timer;
-
         public ParserForm() {
             InitializeComponent();
         }
 
         private async void Form_Load(object sender, EventArgs e) {
-            image = new Bitmap(@"C:\Users\cwalsh\Downloads\Screenshot_20170906-100313.png");
+            const int fps = 1000 / 60;
+
+            var image = new Bitmap(Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), 
+                @"Downloads\Screenshot_20170906-100313.png"));
 
             pictureBox.Image = image;
             pictureBox.Height = Screen.PrimaryScreen.WorkingArea.Height;
@@ -25,24 +26,27 @@ namespace LifeLogGUI
                         
             Top = 0;
             Size = pictureBox.Size;
+            
+            using (var timer = new Timer { Interval = fps })
+            {
+                timer.Tick += (_, __) => pictureBox.Refresh();
+                timer.Start();
 
-            timer = new Timer {
-                Interval = fps,
-            };
-            timer.Tick += (_, __) => pictureBox.Refresh();
-            timer.Start();
-
-            var parser = new Parser(new InvertingTrackingBitmap {
-                KeepCount = 2000,
-                IAsyncBitmap = new DelayedBitmap {
-                    DelayInterval = fps,
-                    DelayCount = 10,
-                    IAsyncBitmap = new WrappingBitmap {
-                        Bitmap = image,
+                var parser = new Parser(new InvertingTrackingBitmap
+                {
+                    KeepCount = 2000,
+                    IAsyncBitmap = new DelayedBitmap
+                    {
+                        DelayInterval = fps,
+                        DelayCount = 10,
+                        IAsyncBitmap = new WrappingBitmap
+                        {
+                            Bitmap = image,
+                        },
                     },
-                },
-            });
-            await parser.Run();
+                });
+                await parser.Run(); 
+            }
         }
     }
 }
